464af2985e1ada856e31c0a70e7c80ffa2415a2b
coryrylan <2021067+coryrylan@users.noreply.github.com>
2023-11-23 21:46:40 -0600

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import 'zone.js';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
